37e7fdaec26bd4d99d1095fbb282366b578338ef
Caro0708 <apinto5@udi.edu.co>
2021-11-04 21:24:41 -0500

    Zip file generate in main url

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -16,6 +16,7 @@ class MainController extends Controller
         $name = isset($_GET['name']) ? $_GET['name'] : null;
         $status = isset($_GET['status']) ? $_GET['status'] : null;
         $gender = isset($_GET['gender']) ? $_GET['gender'] : null;
+        $zip = isset($_GET['zip']) ? $_GET['zip'] : false;
 
         // Genración de URL
         $url = $this->url . ($name ? '?name=' . $name : '');
@@ -25,23 +26,17 @@ class MainController extends Controller
         // Consumo del API y respuesta
         $client = new \GuzzleHttp\Client();
         $response = $client->request('GET', $url);
-        return response()->json(json_decode($response->getBody()), 200);
-    }
-
-    public function zip () {
-        // Consumo del API
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', $this->url);
-        $json_data = $response->getBody();
-
-        // Creación del archivo
-        $zip_file = 'trii.zip';
-        $zip = new \ZipArchive();
-        $zip->open($zip_file, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
-        $zip->addFromString('data.json', $json_data);
-        $zip->close();
-
-        // Respuesta y descarga
-        return response()->download($zip_file);
+        if ($zip) {
+            // Creación del archivo
+            $zip_file = $zip . '.zip';
+            $zip = new \ZipArchive();
+            $zip->open($zip_file, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+            $zip->addFromString('data.json', $response->getBody());
+            $zip->close();
+    
+            return response()->download($zip_file);
+        } else {
+            return response()->json(json_decode($response->getBody()), 200);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,3 @@ use App\Http\Controllers\MainController;
 */
 
 Route::get('/', [MainController::class, 'index']);
-Route::get('/zip', [MainController::class, 'zip']);
